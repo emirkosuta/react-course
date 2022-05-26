f52337a64ea8c16518f4dc7af596d5e3bcca9f6e
emir987 <emir@legionelladossier.com>
2022-05-26 15:25:44 +0200

    use reduce for max value

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import person from './class.js';
 import { filterRestFunction } from './rest-operator.js';
 import * as desctructure_params from './destructure.js';
 import movie from './referenceTypes.js';
+import { numbers, maxValue } from './popular-array-functions.js';
 
 person.printName();
 person.printGender();
@@ -11,3 +12,4 @@ console.log(newNumbers);
 console.log(filterRestFunction(1, 2, 3, 4));
 console.log(desctructure_params, desctructure_params.age);
 console.log(movie.sound());
+console.log(`Max value of array ${numbers} is ${maxValue}`);
