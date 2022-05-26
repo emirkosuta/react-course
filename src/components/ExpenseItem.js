95e8e8125d90f9585ea24c3a1e621d6a6138d002
emir987 <emir@legionelladossier.com>
2022-05-26 23:04:01 +0200

    handle expense dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,17 @@ import './ExpenseItem.css';
 
 export const ExpenseItem = (props) => {
   console.log(props);
+  const year = props.expense.date.getFullYear();
+  const month = props.expense.date.toLocaleString('en-GB', { month: 'long' });
+  const day = props.expense.date.toLocaleString('en-GB', { day: '2-digit' });
   return (
     <div className="expense-item">
-      <div>{props.expense.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+
       <div className="expense-item__description">
         <h2>{props.expense.title}</h2>
         <div className="expense-item__price">${props.expense.price}</div>
