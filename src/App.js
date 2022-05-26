7cbecf7748f5bd5dc7d189d9c4ecf3262ddd7adb
emir987 <emir@legionelladossier.com>
2022-05-27 00:38:23 +0200

    use composition (component isnide of component)

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
 import React from 'react';
-import { ExpenseItem } from './components/ExpenseItem';
+import { Expenses } from './components/Expenses';
 
 function App() {
-  const expenses = [
-    {
-      id: 1,
-      title: 'Car insurance',
-      price: 200,
-      date: new Date(2021, 11, 5),
-    },
-    {
-      id: 2,
-      title: 'Gilette',
-      price: 3.5,
-      date: new Date(2021, 12, 5),
-    },
-    {
-      id: 3,
-      title: 'React course',
-      price: 45,
-      date: new Date(2022, 26, 5),
-    },
-    {
-      id: 4,
-      title: 'Bensedin',
-      price: 6,
-      date: new Date(2022, 26, 5),
-    },
-  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem expense={expenses[0]} />
+      <Expenses />
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { ExpenseItem } from './ExpenseItem';
+import { Card } from './Card';
+import './Expenses.css';
+
+export const Expenses = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Car insurance',
+      price: 200,
+      date: new Date(2021, 11, 5),
+    },
+    {
+      id: 2,
+      title: 'Gilette',
+      price: 3.5,
+      date: new Date(2021, 12, 5),
+    },
+    {
+      id: 3,
+      title: 'React course',
+      price: 45,
+      date: new Date(2022, 26, 5),
+    },
+    {
+      id: 4,
+      title: 'Bensedin',
+      price: 6,
+      date: new Date(2022, 26, 5),
+    },
+  ];
+  return (
+    <Card className="expenses">
+      <ExpenseItem expense={expenses[0]} />
+    </Card>
+  );
+};
