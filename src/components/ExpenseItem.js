356ddde984f91b742060ad4d4dc0108ea6a2f732
emir987 <emir@legionelladossier.com>
2022-05-26 17:53:30 +0200

    expense item component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ExpenseItem.css';
+
+export const ExpenseItem = () => {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$200</div>
+      </div>
+    </div>
+  );
+};
