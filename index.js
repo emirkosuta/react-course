e57229b3f238894ceaaa7b5dc39c5ad0423dab3a
emir987 <emir@legionelladossier.com>
2022-05-26 13:49:09 +0200

    use rest operator

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { newNumbers } from './spread.js';
 import { person } from './class.js';
+import { filterRestFunction } from './rest-operator.js';
 
 person.printName();
 person.printGender();
 
 console.log(newNumbers);
+console.log(filterRestFunction(1, 2, 3, 4));
---
+++ rest-operator.js
@@ -0,0 +1,2 @@
+export const filterRestFunction = (...args) =>
+  args.filter((el) => el === 1 || el === 2);
