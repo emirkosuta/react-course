adf5de7c39104fb6ba7658ee5af9f942933d13ea
emir987 <emir@legionelladossier.com>
2022-05-26 15:27:27 +0200

    remove inital value as there is no need for that.

--- popular-array-functions.js
+++ popular-array-functions.js
@@ -3,4 +3,4 @@ export const numbers = [10, 20, 30, 33, 4, 5, 555, 54, 3, 2, 1, 11, 1];
 export const maxValue = numbers.reduce((previous, current) => {
   if (previous > current) return previous;
   return current;
-}, 0);
+});
