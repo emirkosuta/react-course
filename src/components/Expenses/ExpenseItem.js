25c98712c028eb20c7fd749b921a22eae73ae7e2
emir987 <emir@legionelladossier.com>
2022-05-29 19:59:34 +0200

    move expensesList to separate component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,19 @@ import './ExpenseItem.css';
 import { ExpenseDate } from './ExpenseDate';
 import { Card } from '../UI/Card';
 
-export const ExpenseItem = (props) => {
+const ExpenseItem = (props) => {
   const { title, price, date } = props.expense;
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${price}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${price}</div>
+        </div>
+      </Card>
+    </li>
   );
 };
+
+export default ExpenseItem;
