dc8b6301b83849b050f9c07662c3f5613bcf1ded
emir987 <emir@legionelladossier.com>
2022-05-29 15:28:13 +0200

    make expenseItem component to be presentationsl
    remove button change title functionalty

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseItem.css';
 import { ExpenseDate } from './ExpenseDate';
 import { Card } from '../UI/Card';
 
 export const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.expense.title);
-
-  const onClickHandler = () => {
-    setTitle('Changed!');
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.expense.tittle}}</h2>
         <div className="expense-item__price">${props.expense.price}</div>
-        <button onClick={onClickHandler}>Button</button>
       </div>
     </Card>
   );
