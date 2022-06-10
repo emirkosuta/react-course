d047fbbde891ed3256b2f6307a9d81e1e6903da7
emir987 <emir@legionelladossier.com>
2022-06-10 11:46:18 +0200

    clean console.log

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const Button = (props) => {
-  console.log(props.type);
   return (
     <button
       className={classes.button}
