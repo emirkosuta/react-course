7c651bb914896bd098ca9bbf67a4f82fe0c7eeea
emir987 <emir@legionelladossier.com>
2022-06-10 11:46:48 +0200

    Use portal to render modal at the root level

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Button from './Button';
 import Card from './Card';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onCancel} />;
+};
+
+const Modal = (props) => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.content}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onCancel}>Close</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onCancel} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.content}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onCancel}>Close</Button>
-        </footer>
-      </Card>
-    </div>
+    <>
+      {ReactDOM.createPortal(
+        <Backdrop onCancel={props.onCancel} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          content={props.content}
+          onCancel={props.onCancel}
+        />,
+        document.getElementById('modal-root')
+      )}
+    </>
   );
 };
 
